67e36a9c488989769f039351f1d7186c51cc0edb
MrCat9 <634467045@qq.com>
2022-03-25 11:35:43 +0800

    update

---
+++ 189_1_rotate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        pass
+
+
+if __name__ == '__main__':
+    test_nums = [1, 2, 3, 4, 5, 6, 7]
+    test_k = 3
+
+    test_result = Solution().sortedSquares(test_nums, test_k)
+    # print(test_result)
