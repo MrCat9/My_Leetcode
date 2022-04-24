9be672ce78a5fcd6c39c6e7c9682f05f68d5440f
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2022-04-24 21:43:11 +0800

    update

---
+++ 617_1_mergeTrees.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        root
+        pass
+
+
+if __name__ == '__main__':
+    # Definition for a binary tree node.
+    class TreeNode:
+        def __init__(self, val=0, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+    def gen_tree(node_list: list):
+        pass
+
+    test_root1 = [1, 3, 2, 5]
+    test_root2 = [2, 1, 3, None, 4, None, 7]
+    Solution().mergeTrees(test_root1, test_root2)
